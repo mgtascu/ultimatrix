72676828a4eccbb9c8f13102690244b9c0399bba
mgtascu <m.g.tascu@gmail.com>
2020-02-25 21:07:49 +0200

    add model

--- spend_balancer/api/models.py
+++ spend_balancer/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class ItemType(models.Model):
+    iid = models.IntegerField(default=1, primary_key=True, null=False, unique=True)
+    itype = models.CharField(max_length=50, null=False)
+
+
+class AcqItem(models.Model):
+    iname = models.CharField(max_length=200, null=False)
+    iprice = models.FloatField(null=True)
+    idescription = models.CharField(max_length=3000, null=True)
+    itype = models.ForeignKey(ItemType, on_delete=models.CASCADE)
